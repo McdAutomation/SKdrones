5c137c4ddb2772e2b697207204f86a0134edeb0c
McdAutomation <anupam.trivedy@us.mcd.com>
2018-07-18 17:47:19 +0530

    proper trying

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,3 +129,7 @@ STATICFILES_DIRS = [
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATIC_ROOT = os.path.join(BASE_DIR, "static")
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,  'templates'),
+    # Add to this list all the locations containing your static files
+)
